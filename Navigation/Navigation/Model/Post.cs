47717be0e536944c92f81e3f1ef62ff25825242c
Andoni Moutousis <amoutousis@nexient.com>
2021-11-18 17:29:29 -0500

    Added insert functionality

---
+++ Navigation/Navigation/Model/Post.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Navigation.Model
+{
+    public class Post
+    {
+        [PrimaryKey, AutoIncrement]
+        public int _id { get; set; }
+        [MaxLength(250)]
+        public string _experience { get; set; }
+
+
+    }
+}
--- Navigation/Navigation/NewTravelPage.xaml.cs
+++ Navigation/Navigation/NewTravelPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Navigation.Model;
+using SQLite;
 
 namespace Navigation
 {
@@ -15,6 +17,29 @@ namespace Navigation
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            Post post = new Post()
+            {
+                _experience = experienceEntry.Text
+            };
+
+            SQLiteConnection conn = new SQLiteConnection(App._databaseLocation);
+            conn.CreateTable<Post>();
+            int rows = conn.Insert(post);
+            conn.Close();
+
+            if (rows > 0)
+            {
+                DisplayAlert("Success", "Experience Successfully Inserted", "OK");)
+            }
+            else
+            {
+                DisplayAlert("Failure", "Experience Failed", "OK");
+            }
+
         }
     }
 }
