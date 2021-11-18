51ce4f78a11f5a70236188f1b767c38c3d9416d8
Andoni Moutousis <amoutousis@nexient.com>
2021-11-18 17:02:19 -0500

    first commit

---
+++ Navigation/Navigation.iOS/AppDelegate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+using Foundation;
+using UIKit;
+
+namespace Navigation.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+
+            string dbName = "travel_db.sqlite";
+
+            //using the System.Environment class, we can get the location of where the db will be stored
+            //we cannot us the personal folder as Apple does not allow this so we look in the directory above Personal -- Library
+            string folderPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "..", "Library");
+
+            //combing both folderpath and the dbName
+            string fullPath = Path.Combine(folderPath, dbName);
+
+            LoadApplication(new App(fullPath));
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
---
+++ Navigation/Navigation/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation
+{
+    public partial class App : Application
+    {
+        public static string _databaseLocation = string.Empty;
+        public App()
+        {
+            InitializeComponent();
+
+            //this creates a parent page that displays the main page and utilize a Navigation page
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        public App(string databaseLocation)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            _databaseLocation = databaseLocation;
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
